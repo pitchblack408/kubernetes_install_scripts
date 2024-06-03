8993c3be8b286a0cfaa4ba35c63dcab401d79683
unknown <8700224+pitchblack408@users.noreply.github.com>
2024-06-03 10:29:47 -0700

    node3 wrong ip

--- multipass_create_worker_node3.bat
+++ multipass_create_worker_node3.bat
@@ -5,7 +5,7 @@ set cpu_count=2
 set memory_size=2048M
 set mac="52:54:00:74:d6:e3"
 set network_params=name=multipassbridge,mode=manual,mac=%mac%
-set static_ip=192.168.20.14
+set static_ip=192.168.20.15
 
 multipass launch --name %instance_name% -c %cpu_count% -m %memory_size% --network %network_params%
 multipass exec %instance_name% -- wget https://raw.githubusercontent.com/pitchblack408/kubernetes_install_scripts/main/install_k8_base.sh
